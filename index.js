a36d61dd12b50834bafe9e122f7b78c91d29f099
Julia Wey-Burgoyne <juliawey@gmail.com>
2023-09-20 20:44:35 -0400

    Adds find lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(array) {
+    let result = undefined
+    for(let item of array) {
+        if(item.result === "W") {
+            return item.year
+        }
+    }
+}
